f95b26252fd0071c6e1de322c25d07b3798777d0
sana22mav <sana22_mav@student.ztu.edu.ua>
2022-11-12 15:35:57 +0200

    Last 2 outputs changed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,5 +2,5 @@
 Console.WriteLine("Hello");
 Console.WriteLine("I am street photographer");
 Console.WriteLine("Can I have some pictures of you");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Your outfit is amazing");
+Console.WriteLine("Yes, sure");
