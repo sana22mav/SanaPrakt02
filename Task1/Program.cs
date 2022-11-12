0b13377bfbd8a8cfafb659dc77732375e403a921
sana22mav <sana22_mav@student.ztu.edu.ua>
2022-11-12 15:31:27 +0200

    Output fixed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 
