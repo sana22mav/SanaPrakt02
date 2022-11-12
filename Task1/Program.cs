bf2411e2d9bec080e29f04213c533471666427d3
sana22mav <sana22_mav@student.ztu.edu.ua>
2022-11-12 15:35:15 +0200

    First 3 outputs changed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Hello");
+Console.WriteLine("I am street photographer");
+Console.WriteLine("Can I have some pictures of you");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
